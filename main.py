64b24d7f1780571b6795e78f41c536e2504d6769
Suyash <suyash.d@netwebindia.com>
2023-03-10 13:10:57 +0530

    Update project management files after installing requests library

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+"""The main file responsible for running invoking the tasks and the
+workflows around them.
+"""
 import tasks
 
 if __name__ == "__main__":
-    result = tasks.add.delay(5, 10)
-    while not (readiness := result.ready()):
-        print(f"Result: {readiness}")
-    print(f"Is Result Ready: {readiness}")
-    print(f"Result: {result.get()}")
+    pass
