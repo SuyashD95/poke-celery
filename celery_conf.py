1e464e35d0429c1cc6b7c49fd202ccade536292c
Suyash <suyash.d@netwebindia.com>
2023-03-13 15:33:07 +0530

    Make changes to Celery configuration to use Redis as result backend

--- celery_conf.py
+++ celery_conf.py
@@ -5,7 +5,7 @@ from celery import Celery
 app = Celery(
     "celery_proj",
     broker="amqp://localhost",
-    backend="rpc://",
+    backend="redis://localhost:6379/0",
     include=["tasks"],
 )
 
