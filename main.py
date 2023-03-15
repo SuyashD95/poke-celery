a75a159ffbfc02b2445fa42cc2ec12ecbba6d6bd
Suyash <suyash.d@netwebindia.com>
2023-03-15 11:00:51 +0530

    Use starmap primitive to run task sequentially by iterating over its arguments which are written in *args format

--- main.py
+++ main.py
@@ -72,6 +72,37 @@ def print_pokemon_summaries_using_map(names: list[str]):
     )
 
 
+def print_pokemon_summaries_using_starmap(names: list[str]):
+    """Print summarized information about specified pokemons.
+
+    NOTE: This function will use starmap primitive and thus will
+    be executed sequentially.
+
+    Parameters
+    ----------
+    names: List of pokemon names.
+    """
+    pkmn_summary_result: AsyncResult = tasks.stringify_pokemon_summary.starmap(
+        zip(names)
+    ).apply_async()
+
+    while not pkmn_summary_result.ready():
+        print("Task to get pokemon summaries is still in progress.")
+        sleep(1)
+
+    failed = 0
+    for index, result in enumerate(pkmn_summary_result.get()):
+        print(f"Pokemon Name: {names[index]}")
+        if result:
+            print(f"  {result}")
+        else:
+            print("  No information was found.")
+            failed += 1
+    print(
+        f"\nSummary: total = {len(names)}, succeeded = {len(names) - failed}, failed = {failed}"
+    )
+
+
 if __name__ == "__main__":
     pokemons = [
         "pikachu",
@@ -97,3 +128,8 @@ if __name__ == "__main__":
     print_pokemon_summaries_using_map(pokemons)
     end_time = time()
     print(f"Execution time (map): {end_time - start_time}")
+
+    start_time = time()
+    print_pokemon_summaries_using_starmap(pokemons)
+    end_time = time()
+    print(f"Execution time (starmap): {end_time - start_time}")
